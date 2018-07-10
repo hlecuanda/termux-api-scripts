45fe990c2f75a0d143bc94d79759f3c3cd428634
HÃctor Lecuanda (H-Lo) <hector@lecuanda.com>
2018-07-10 10:51:01 +0000

    added dependency on click for parsing CLI'

--- sms.py
+++ sms.py
@@ -1,16 +1,22 @@
 # vim: number nowrap
 
 from time import sleep
+import click
 import subprocess
 import time
 
 COUNTDOWN='500'
-DESTINATION='6461508444'
-while True:
-    time.sleep(1)
-    smscontent: "washusi eashuwa"
-    subprocess.popen('termux-sms-send','-n',DESTINATION, smscontent)
-    print('       {}  '.format(COUNTDOWN)) 
-    countdown =-1
+DESTINATION=''
+@click.command()
+@click.option()
+def sms():
+    ''' periodically sends an sms message to a number '''
+    while True:
+        time.sleep(1)
+        smscontent: "washusi eashuwa"
+        subprocess.popen('termux-sms-send','-n',DESTINATION, smscontent)
+        print('       {}  '.format(COUNTDOWN)) 
+        countdown =-1
 
-
+if __name__ == "__main__":
+    sms()
