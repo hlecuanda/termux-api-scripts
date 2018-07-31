20f88be09f47fc12a6a578b22ef521df7bcb4d94
Hector <hector@lecuanda.com>
2018-07-31 16:56:50 -0700

    Tue Jul 31 16:56:50 PDT 2018 WIP commit

--- Makefile
+++ Makefile
@@ -5,4 +5,21 @@ PROGRAM= cheers.py
 #BINGRP=
 #BINMODE=
 BINDIR=$(HOME)/bin
-BINNAME=cheers
+BINNAME=cheer
+
+
+.ifndef MESSAGE
+MESSAGE!=date
+MESSAGE+= WIP commit
+.endif
+
+commit:
+	@echo "commiting"
+	git commit -m "$(MESSAGE)"
+
+commit-all: add-all
+	@git commit -m "$(MESSAGE)"
+
+add-all:
+	@git add .
+
--- cheers.py
+++ cheers.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
 
-cheer='''*\o\* ¡dame una C! *\o/* ¡dame una O! */o/*
-*\o\* ¡Dame una G! *\o/*  ¡Dame una E! */o/*
-*\o\* ¡Dame una R! *\o/*  ¿Que dice? */o/*
-*\o\* ¡COGER! *\o/*  ¡NO SE OYE! */o/* ¡COGER! 
-*\o\* ¡MAS FUERTE¡ *\o/* ¡COGER! */o/*
-*\o\* ¡3 VECES! *\o/* ¡COGER COGER COGER! */o/* WOOOHi'''
+cheer_left='*\o\*'
+cheer_center='*\o/*'
+cheer_right='*/o/*'
 
-print(cheer)
+gimme='¡dame una {}!'
+given='¡{}!'
+
+def pegar(pegamento,pedazos):
+    pegado=''
+    for pedazo in pedazos:
+        pegado += pedazo + pegamento            
+    return pegado
+
+def cheerletra(letra):
+    ch=[]
+    ch.append(cheer_left)
+    ch.append(gimme.format(letra)) 
+    ch.append(cheer_center)
+    ch.append(given.format(letra))
+    ch.append(cheer_right)
+    cheer=pegar(' ',ch)
+    print(cheer)
+
+def spell(frase):
+    for letra in frase.upper():
+        if letra != ' ':
+            cheerletra(letra)
+
+
+if __name__ == '__main__':
+    spell('hola nena')
+
+# vim: ft=python :   
+
